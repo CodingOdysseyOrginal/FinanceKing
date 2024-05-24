fac635344f6994e8a01e0c028a9c4296e39d437d
Christopher O'Connor <christopher.oconnor1@hotmail.com>
2024-05-24 10:05:56 +0100

    user

---
+++ api/Migrations/20240524085249_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "58049876-2643-4e3c-beeb-d30fae7e569a", null, "Admin", "ADMIN" },
+                    { "8af4f866-9c96-42db-a610-7f89aac20d15", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "58049876-2643-4e3c-beeb-d30fae7e569a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8af4f866-9c96-42db-a610-7f89aac20d15");
+        }
+    }
+}
