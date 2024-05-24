d03a654e54f8b8f1eb4ed3ec567095a3b07fc4d6
Christopher O'Connor <christopher.oconnor1@hotmail.com>
2024-05-24 16:55:37 +0100

    Tokens

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9942aed2a3383c719ced4eecb4de260abb0dead")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac635344f6994e8a01e0c028a9c4296e39d437d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
