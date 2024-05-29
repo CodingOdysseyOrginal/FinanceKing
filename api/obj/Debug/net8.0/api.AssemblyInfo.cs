4a77b5ff6b21a0d07ce7bc8db65b56822a23c23a
Christopher O'Connor <christopher.oconnor1@hotmail.com>
2024-05-29 21:01:18 +0100

    Protected Routes/ Logout

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24679db2f808c959147c819d21485130c878db5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aecd5bead372e3167946715f8e88022f9e398c20")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
